423dde2f64f7617205dfb9faea685f25bb7ea40b
alex7186 <sasha2649058@mail.ru>
2020-07-05 22:59:11 +0300

    Create forms.py

---
+++ raw_data/forms.py
@@ -0,0 +1,14 @@
+import json, requests
+def forms (s_input): # s is string
+    url = "https://ws3.morpher.ru/russian/declension"
+    params = dict (
+    s=s_input,
+    format="json"
+    )
+    response = requests.get(url=url, params=params)
+    data = json.loads(response.text)
+    keys = []
+    for acc, key in data.items():
+        keys += [key]
+    keys.pop(-1)
+    return (keys)
